f75e4a09ec493def374fdc0d323c79d6fed10e28
Colum <15142434@studentmail.ul.ie>
2019-03-22 23:08:51 +0000

    admin requests and upload request

--- src/app/services/admin-api.service.ts
+++ src/app/services/admin-api.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders, HttpErrorResponse, HttpParams} from '@angular/common/http';
+import {Observable} from 'rxjs';
 
+const  url = 'https://colum-mullally-fyp-webservice.herokuapp.com';
+const apiUrl = url + '/v1/admin';
 @Injectable({
   providedIn: 'root'
 })
 export class AdminApiService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  getAll(): Observable<any> {
+    return this.http.get(apiUrl + '/allusers',
+      {headers: new HttpHeaders().set('Authorization', 'Basic ' + sessionStorage.getItem('credentials'))});
+  }
+  promoteUser(username: string): Observable<any> {
+    return this.http.post(apiUrl + '/promote' + '?username=' + username,
+      {headers: new HttpHeaders().set('Authorization', 'Basic ' + sessionStorage.getItem('credentials'))});
+  }
 }
--- src/app/services/storage-api.service.ts
+++ src/app/services/storage-api.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders, HttpErrorResponse, HttpParams} from '@angular/common/http';
+import {Observable} from 'rxjs';
 
+const  url = 'https://colum-mullally-fyp-webservice.herokuapp.com';
+const apiUrl = url + '/v1/storage';
 @Injectable({
   providedIn: 'root'
 })
 export class StorageApiService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  fileUpload(file: File ): Observable<any> {
+    const fd = new FormData();
+    fd.append('file', file, file.name)
+    return this.http.post(apiUrl + '/uploadFile', fd,
+      {headers: new HttpHeaders().set('Authorization', 'Basic ' + sessionStorage.getItem('credentials'))});
+  }
 }
